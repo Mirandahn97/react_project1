4760c307ad52cc3f4b1caf326bd320fc0cb7bca7
Mirandahn97 <mira0249@elev.techcollege.dk>
2026-01-05 11:09:40 +0100

    v1

---
+++ first_project/src/App.tsx
@@ -0,0 +1,35 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <section>
+        <h1>Fordele og ulemper ved at bruge React</h1>
+        <section id='list'>
+          <ul>
+            <h2>Fordele</h2>
+            <li>Bedre til større projekter</li>
+            <li>Letter at genbruge kode</li>
+            <li>velligeholdes</li>
+            <li>Skalerbart</li>
+            <li>Ui opdatering</li>
+            <li>Bedre struktur</li>
+          </ul>
+       
+        <ul>
+          <h2>Ulemper</h2>
+          <li>Ikke så god til mindre projekter</li>
+          <li>Komplexitet</li>
+          <li>Skal opdateres</li>
+          <li>Kræver man kan js</li>
+          <li>Ikke så udbredet som jquery</li>
+        </ul>
+        </section >
+      </section>
+    </>
+  )
+}
+
+export default App
